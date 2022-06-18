ba3ba2a07de4e39bf4df95ea87b34e9d7d961b6e
deardaddy <sergpomorsky@gmail.com>
2022-06-19 00:01:32 +0300

    Add task_1  Please enter the commit messae for your changes. Lines starting

---
+++ task_1/Program.cs
@@ -0,0 +1,16 @@
+﻿// напишите программу, которая выводит случайное число из отрезка [10,99]
+// и показывает наибольшую цифру числа
+// 78 - 8
+// 12 -2
+int number = new Random().Next(10,99);
+Console.WriteLine($"Наше случайное число = {number}");
+int digit1 = number / 10;
+int digit2 = number % 10;
+if (digit1 > digit2)
+{
+Console.WriteLine($"цифра {digit1} наибольшая");
+}
+else
+{
+Console.WriteLine($" цифра {digit2} наибольшая");
+}
