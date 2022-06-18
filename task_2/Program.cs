c0d154feed7e402689051ca954e01c800791244a
deardaddy <sergpomorsky@gmail.com>
2022-06-19 00:20:53 +0300

    add task_2  Please enter the commit message for your changes. Lines starting

---
+++ task_2/Program.cs
@@ -0,0 +1,9 @@
+﻿// написать программу, которая выводит случайное трехзначное число 
+//и удаляет вторую цифру этого числа
+int number = new Random().Next(100,999);
+Console.WriteLine($"Наше случайное число = {number}");
+int numberA = number/100;
+int numberB = number/10 % 10;
+int numberC = number % 10;
+int result = numberA * 10 + numberC;
+Console.WriteLine (result);
