6013c629a6c48e71696c3ed1b3f7a797c6931c10
deardaddy <sergpomorsky@gmail.com>
2022-06-21 00:47:24 +0300

    add task_7_homework

---
+++ task_7_homework/Program.cs
@@ -0,0 +1,38 @@
+﻿// Напишите программу, которая принbмает на вход цифру, 
+//обозначающую день недели, и проверяет, является ли этот день выходным
+
+Console.WriteLine("Set number day");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+if (number == 1)
+{
+    Console.WriteLine("no");
+}
+if (number == 2)
+{
+    Console.WriteLine("no");
+}
+if (number == 3)
+{
+    Console.WriteLine("no");
+}
+if (number == 4)
+{
+    Console.WriteLine("no");
+}
+if (number == 5)
+{
+    Console.WriteLine("no");
+}
+if (number == 6)
+{
+    Console.WriteLine("yes");
+}
+if (number == 7)
+{
+    Console.WriteLine("yes");
+}
+if (number > 7 || number == 0)
+{
+    Console.WriteLine("go to work!");
+}
